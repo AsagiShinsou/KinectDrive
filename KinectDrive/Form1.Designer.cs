c0c469200b03151567fdb2e0778acfdf8fb5d8f7
AsagiShinsou <exilecms@gmail.com>
2019-03-18 08:21:05 +0300

    small updates
    Added:
    Kinect methods and defines;
    Modified, but still not working sockets layer.

--- KinectDrive/Form1.Designer.cs
+++ KinectDrive/Form1.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.программаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.kinectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.обновитьСтатусToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.просмотретьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.ServerUrl = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.просмотретьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ServerUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.KinectTimer = new System.Windows.Forms.Timer(this.components);
+            this.klog = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -71,12 +74,12 @@
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(106, 6);
             // 
             // выйтиToolStripMenuItem
             // 
             this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
-            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
             this.выйтиToolStripMenuItem.Text = "Выйти";
             // 
             // kinectToolStripMenuItem
@@ -91,8 +94,16 @@
             // обновитьСтатусToolStripMenuItem
             // 
             this.обновитьСтатусToolStripMenuItem.Name = "обновитьСтатусToolStripMenuItem";
-            this.обновитьСтатусToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.обновитьСтатусToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
             this.обновитьСтатусToolStripMenuItem.Text = "Обновить статус";
+            this.обновитьСтатусToolStripMenuItem.Click += new System.EventHandler(this.обновитьСтатусToolStripMenuItem_Click);
+            // 
+            // просмотретьToolStripMenuItem
+            // 
+            this.просмотретьToolStripMenuItem.Name = "просмотретьToolStripMenuItem";
+            this.просмотретьToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.просмотретьToolStripMenuItem.Text = "Просмотреть";
+            this.просмотретьToolStripMenuItem.Click += new System.EventHandler(this.просмотретьToolStripMenuItem_Click);
             // 
             // оПрограммеToolStripMenuItem
             // 
@@ -102,7 +113,8 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.klog);
+            this.groupBox1.Controls.Add(this.StatusLabel);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.ServerUrl);
@@ -110,43 +122,19 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 53);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(409, 233);
+            this.groupBox1.Size = new System.Drawing.Size(409, 329);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Подключение";
             // 
-            // label1
+            // StatusLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "URL адрес сервера";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Токен доступа";
-            // 
-            // ServerUrl
-            // 
-            this.ServerUrl.Location = new System.Drawing.Point(26, 64);
-            this.ServerUrl.Name = "ServerUrl";
-            this.ServerUrl.Size = new System.Drawing.Size(347, 20);
-            this.ServerUrl.TabIndex = 2;
-            this.ServerUrl.Text = "ws://mailshark.ru:3000";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(26, 116);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(347, 20);
-            this.textBox2.TabIndex = 3;
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(26, 191);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(67, 13);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Статус: N\\A";
             // 
             // button1
             // 
@@ -158,27 +146,56 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
+            // textBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 191);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Статус: N\\A";
+            this.textBox2.Location = new System.Drawing.Point(26, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(347, 20);
+            this.textBox2.TabIndex = 3;
             // 
-            // просмотретьToolStripMenuItem
+            // ServerUrl
             // 
-            this.просмотретьToolStripMenuItem.Name = "просмотретьToolStripMenuItem";
-            this.просмотретьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.просмотретьToolStripMenuItem.Text = "Просмотреть";
-            this.просмотретьToolStripMenuItem.Click += new System.EventHandler(this.просмотретьToolStripMenuItem_Click);
+            this.ServerUrl.Location = new System.Drawing.Point(26, 64);
+            this.ServerUrl.Name = "ServerUrl";
+            this.ServerUrl.Size = new System.Drawing.Size(347, 20);
+            this.ServerUrl.TabIndex = 2;
+            this.ServerUrl.Text = "ws://mailshark.ru:3000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Токен доступа";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL адрес сервера";
+            // 
+            // KinectTimer
+            // 
+            this.KinectTimer.Tick += new System.EventHandler(this.KinectTimer_Tick);
+            // 
+            // klog
+            // 
+            this.klog.Location = new System.Drawing.Point(26, 248);
+            this.klog.Name = "klog";
+            this.klog.Size = new System.Drawing.Size(347, 52);
+            this.klog.TabIndex = 6;
+            this.klog.Text = "";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 308);
+            this.ClientSize = new System.Drawing.Size(432, 435);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -187,6 +204,7 @@
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.RightToLeftLayout = true;
             this.Text = "KinectDrive v 0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -212,8 +230,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label StatusLabel;
         private System.Windows.Forms.ToolStripMenuItem просмотретьToolStripMenuItem;
+        private System.Windows.Forms.Timer KinectTimer;
+        private System.Windows.Forms.RichTextBox klog;
     }
 }
 
--- KinectDrive/Form1.cs
+++ KinectDrive/Form1.cs
@@ -11,12 +11,17 @@ using System.Net;
 using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Threading;
+using KinectManagement;
+using Microsoft.Kinect;
 
 
 namespace KinectDrive
 {
+    
     public partial class MainForm : Form
     {
+        private KinectSensor sensor;
+        public KinectForm kform;
         public MainForm()
         {
             InitializeComponent();
@@ -29,13 +34,13 @@ namespace KinectDrive
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            kform = new KinectForm();
         }
 
         private void просмотретьToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var KinectForm = new KinectForm();
-            KinectForm.Show();
+            
+            kform.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -43,7 +48,67 @@ namespace KinectDrive
 
             var socket = new ClientWebSocket();
             socket.ConnectAsync(new Uri(ServerUrl.Text), CancellationToken.None);
-            socket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            var encoded = Encoding.UTF8.GetBytes("hi there");
+            var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
+            socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            
+            //socket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        private void обновитьСтатусToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                this.sensor.SkeletonStream.Enable();
+                StatusLabel.Text = "Kinect connected!";
+                this.sensor.Start();
+                
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+                this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                
+                //this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+            
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void KinectTimer_Tick(object sender, EventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                
+            }
+        }
+
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            
+            Skeleton[] skeletons = new Skeleton[0];
+            if (skeletons.Length != 0)
+            {
+                klog.Text = "SKEL FOUND";
+                foreach (Skeleton skel in skeletons)
+                {
+                    
+                }
+            }
         }
     }
 }
